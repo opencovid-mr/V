87d60d50c9d068c0f4196a48b42c32b25dece79f
lvegro <lorenzo.vegro@gmail.com>
2021-01-02 11:06:41 +0100

    1) creata funzione per convertire json in csv 2) cambiata naming convention file (data+nomefile) per facilitare sort

--- script.py
+++ script.py
@@ -2,6 +2,13 @@ import requests
 import json
 import csv
 
+def write_json_to_csv(update, filename, header, data):
+    filename = "data/"+ update + "_" + filename + ".csv"
+    f = csv.writer(open(filename, "w+"),lineterminator='\n')
+    f.writerow(header)
+    for row in data:    
+        f.writerow(row['C'])
+
 headers = {
     'Connection': 'keep-alive',
     'Accept': 'application/json, text/plain, */*',
@@ -22,17 +29,19 @@ params = (
     ('synchronous', 'true'),
 )
 
+url = 'https://wabi-europe-north-b-api.analysis.windows.net/public/reports/querydata'
+
 data = '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"TAB_MASTER","Type":0},{"Name":"t1","Entity":"TAB_MASTER_PIVOT","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TML_FASCIA_ETA"},"Name":"TAB_MASTER.TML_FASCIA_ETA"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t1"}},"Property":"Valore"}},"Function":0},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TML_FASCIA_ETA"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1]}]},"DataReduction":{"DataVolume":4,"Primary":{"Window":{"Count":1000}}},"Version":1}}}]},"CacheKey":"{\\"Commands\\":[{\\"SemanticQueryDataShapeCommand\\":{\\"Query\\":{\\"Version\\":2,\\"From\\":[{\\"Name\\":\\"t\\",\\"Entity\\":\\"TAB_MASTER\\",\\"Type\\":0},{\\"Name\\":\\"t1\\",\\"Entity\\":\\"TAB_MASTER_PIVOT\\",\\"Type\\":0}],\\"Select\\":[{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t\\"}},\\"Property\\":\\"TML_FASCIA_ETA\\"},\\"Name\\":\\"TAB_MASTER.TML_FASCIA_ETA\\"},{\\"Aggregation\\":{\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t1\\"}},\\"Property\\":\\"Valore\\"}},\\"Function\\":0},\\"Name\\":\\"Sum(TAB_MASTER_PIVOT.Valore)\\"}],\\"OrderBy\\":[{\\"Direction\\":1,\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t\\"}},\\"Property\\":\\"TML_FASCIA_ETA\\"}}}]},\\"Binding\\":{\\"Primary\\":{\\"Groupings\\":[{\\"Projections\\":[0,1]}]},\\"DataReduction\\":{\\"DataVolume\\":4,\\"Primary\\":{\\"Window\\":{\\"Count\\":1000}}},\\"Version\\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}'
 
-FASCIA_ETA = json.loads(requests.post('https://wabi-europe-north-b-api.analysis.windows.net/public/reports/querydata', headers=headers, params=params, data=data).text)
+FASCIA_ETA = json.loads(requests.post(url, headers=headers, params=params, data=data).text)
 
 data = '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t2","Entity":"TAB_MASTER_PIVOT","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Categoria Attributo"},"Name":"TAB_MASTER_PIVOT.Categoria Attributo"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Valore"}},"Function":0},"Name":"Sum(TAB_MASTER_PIVOT.Valore)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"Categoria Attributo"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1]}]},"DataReduction":{"DataVolume":4,"Primary":{"Window":{"Count":1000}}},"Version":1}}}]},"CacheKey":"{\\"Commands\\":[{\\"SemanticQueryDataShapeCommand\\":{\\"Query\\":{\\"Version\\":2,\\"From\\":[{\\"Name\\":\\"t2\\",\\"Entity\\":\\"TAB_MASTER_PIVOT\\",\\"Type\\":0}],\\"Select\\":[{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t2\\"}},\\"Property\\":\\"Categoria Attributo\\"},\\"Name\\":\\"TAB_MASTER_PIVOT.Categoria Attributo\\"},{\\"Aggregation\\":{\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t2\\"}},\\"Property\\":\\"Valore\\"}},\\"Function\\":0},\\"Name\\":\\"Sum(TAB_MASTER_PIVOT.Valore)\\"}],\\"OrderBy\\":[{\\"Direction\\":1,\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t2\\"}},\\"Property\\":\\"Categoria Attributo\\"}}}]},\\"Binding\\":{\\"Primary\\":{\\"Groupings\\":[{\\"Projections\\":[0,1]}]},\\"DataReduction\\":{\\"DataVolume\\":4,\\"Primary\\":{\\"Window\\":{\\"Count\\":1000}}},\\"Version\\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}'
 
-CATEGORIA = json.loads(requests.post('https://wabi-europe-north-b-api.analysis.windows.net/public/reports/querydata', headers=headers, params=params, data=data).text)
+CATEGORIA = json.loads(requests.post(url, headers=headers, params=params, data=data).text)
 
 data = '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t2","Entity":"TAB_REGIONI","Type":0},{"Name":"t","Entity":"TAB_MASTER","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"AREA"},"Name":"TAB_REGIONI.AREA"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TOT_SOMM"}},"Function":0},"Name":"Sum(TAB_MASTER.TOT_SOMM)"},{"Measure":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"TassoVaccinazione"},"Name":"TAB_MASTER.TassoVaccinazione"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"DOSI_CONSEGNATE"}},"Function":4},"Name":"Sum(TAB_MASTER.DOSI_CONSEGNATE)"}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t2"}},"Property":"AREA"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3]}]},"DataReduction":{"DataVolume":3,"Primary":{"Window":{"Count":500}}},"Version":1}}}]},"CacheKey":"{\\"Commands\\":[{\\"SemanticQueryDataShapeCommand\\":{\\"Query\\":{\\"Version\\":2,\\"From\\":[{\\"Name\\":\\"t2\\",\\"Entity\\":\\"TAB_REGIONI\\",\\"Type\\":0},{\\"Name\\":\\"t\\",\\"Entity\\":\\"TAB_MASTER\\",\\"Type\\":0}],\\"Select\\":[{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t2\\"}},\\"Property\\":\\"AREA\\"},\\"Name\\":\\"TAB_REGIONI.AREA\\"},{\\"Aggregation\\":{\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t\\"}},\\"Property\\":\\"TOT_SOMM\\"}},\\"Function\\":0},\\"Name\\":\\"Sum(TAB_MASTER.TOT_SOMM)\\"},{\\"Measure\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t\\"}},\\"Property\\":\\"TassoVaccinazione\\"},\\"Name\\":\\"TAB_MASTER.TassoVaccinazione\\"},{\\"Aggregation\\":{\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t\\"}},\\"Property\\":\\"DOSI_CONSEGNATE\\"}},\\"Function\\":4},\\"Name\\":\\"Sum(TAB_MASTER.DOSI_CONSEGNATE)\\"}],\\"OrderBy\\":[{\\"Direction\\":1,\\"Expression\\":{\\"Column\\":{\\"Expression\\":{\\"SourceRef\\":{\\"Source\\":\\"t2\\"}},\\"Property\\":\\"AREA\\"}}}]},\\"Binding\\":{\\"Primary\\":{\\"Groupings\\":[{\\"Projections\\":[0,1,2,3]}]},\\"DataReduction\\":{\\"DataVolume\\":3,\\"Primary\\":{\\"Window\\":{\\"Count\\":500}}},\\"Version\\":1}}}]}","QueryId":"","ApplicationContext":{"DatasetId":"5bff6260-1025-49e0-8e9b-169ade7c07f9","Sources":[{"ReportId":"b548a77c-ab0a-4d7c-a457-2e38c2914fc6"}]}}],"cancelQueries":[],"modelId":4280811}'
 
-REGIONI =  json.loads(requests.post('https://wabi-europe-north-b-api.analysis.windows.net/public/reports/querydata', headers=headers, params=params, data=data).text)
+REGIONI =  json.loads(requests.post(url, headers=headers, params=params, data=data).text)
 
 
 data_reg=REGIONI["results"][0]["result"]['data']['dsr']['DS'][0]['PH'][0]['DM0']
@@ -45,24 +54,6 @@ update=FASCIA_ETA["results"][0]["result"]['data']['timestamp']
 
 update=update.replace(':', '_').replace('.', '_')
 
-
-f_r = csv.writer(open("data/regioni"+update+".csv", "w+"),lineterminator='\n')
-
-f_r.writerow(["Regione", "somministrazioni", "percentuale", "dosi_consegnate"])
-
-for row in data_reg:
-    f_r.writerow(row['C'])
-
-f_c = csv.writer(open("data/categorie"+update+".csv", "w+"),lineterminator='\n')
-
-f_c.writerow(["Categoria", "somministrazioni"])
-
-for row in data_cat:
-    f_c.writerow(row['C'])
-
-f_e = csv.writer(open("data/eta"+update+".csv", "w+"),lineterminator='\n')
-
-f_e.writerow(["Classe", "somministrazioni"])
-
-for row in data_eta:
-    f_e.writerow(row['C'])
+write_json_to_csv(update, "regioni", ["Regione", "somministrazioni", "percentuale", "dosi_consegnate"],  data_reg)
+write_json_to_csv(update, "categoria", ["Categoria", "somministrazioni"],  data_cat)
+write_json_to_csv(update, "eta", ["Classe", "somministrazioni"],  data_eta)
